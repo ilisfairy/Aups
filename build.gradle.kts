2907ecf35e57d2bd6e91062a2cd4920e4c177772
Aiuna <ilisfairy@outlook.com>
2022-08-12 07:44:47 -0400

    bump version 0.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    val kotlinVersion = "1.7.0"
+    val kotlinVersion = "1.7.10"
     kotlin("jvm") version kotlinVersion
     id("net.mamoe.mirai-console") version "2.12.1"
 }
 
 group = "moe.naynna"
-version = "0.0.1"
+version = "0.1.0"
 
 repositories {
     mavenLocal()
@@ -13,4 +15,13 @@ repositories {
 }
 dependencies{
     implementation("com.google.code.gson:gson:2.9.1")
+    implementation(kotlin("stdlib-jdk8"))
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
